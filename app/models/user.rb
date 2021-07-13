833f9f4c3c190c2a3ce59049fd6e48ab87d4c98b
Teddy <livistteddy@gmail.com>
2021-07-13 07:30:52 +0100

    Generate article model and r/ship with author(user model)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :articles, foreign_key: 'author_id'
+
 end
