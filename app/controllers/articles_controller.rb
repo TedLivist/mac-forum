b1e465d12c329016105d5cac47bc20f1d35fe771
Teddy <livistteddy@gmail.com>
2021-07-13 12:46:16 +0100

    Add article category option in new article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :category_ids)
   end
 
 end
