338e9f3c2df94f34190cd4fa8fdc3da3c545d519
Teddy <livistteddy@gmail.com>
2021-07-16 11:49:39 +0100

    Fix linter errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
+# rubocop:disable Style/ConditionalAssignment
+
 class HomeController < ApplicationController
   def index
-
-    if Article.count > 0
-      @top_vote_article = Article.find(Vote.group(:article_id).count.max_by{|k, v| v}[0])
+    if Article.count.positive?
+      @top_vote_article = Article.find(Vote.group(:article_id).count.max_by { |_k, v| v }[0])
     else
       @top_vote_article = nil
     end
-    
   end
 end
+
+# rubocop:enable Style/ConditionalAssignment
