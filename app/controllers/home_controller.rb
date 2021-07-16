457ceea97fdf0344a255386666660dd0c37c4b6f
Teddy <livistteddy@gmail.com>
2021-07-16 11:05:45 +0100

    Update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @top_vote_article = Article.find(Vote.group(:article_id).count.max_by{|k, v| v}[0])
+
+    if Article.count > 0
+      @top_vote_article = Article.find(Vote.group(:article_id).count.max_by{|k, v| v}[0])
+    else
+      @top_vote_article = nil
+    end
+    
   end
 end
