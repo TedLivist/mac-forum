1f3e5754eb6da2d903c842907718a0a2ae9d5291
Teddy <livistteddy@gmail.com>
2021-07-18 15:37:36 +0100

    Add helper method for category show page

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  def cate_articles(category, category_articles)
+    if !category_articles.empty?
+      render 'categories/cate_article', obj: category
+    else
+      "There are no articles in this category"
+    end
+  end
 end
