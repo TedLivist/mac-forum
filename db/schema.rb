f11658ed776ce6452b5b2fa393c49c2c80d1cc89
TedLivist <livistteddy@gmail.com>
2021-07-19 13:18:08 +0100

    Add instructions for bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 2021_07_16_062836) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false
     t.boolean "adminn", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
